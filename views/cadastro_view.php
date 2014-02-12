9059ff06490574d4513c29f40cd08ed93924836e
rafa <rafa@10.0.96.50>
2014-02-12 11:46:07 -0200

    Test - (Comit Net Beans)

--- views/cadastro_view.php
+++ views/cadastro_view.php
@@ -3,3 +3,4 @@ $this->load->view('includes/header');
 $this->load->view('includes/menu');
 if($tela!='') $this->load->view('telas/'.$tela);
 $this->load->view('includes/footer');
+echo "teste net beans";
