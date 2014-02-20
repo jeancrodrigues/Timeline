54e6194b52d5b0508b2a312098a60f2d2f290427
jeanrock <jeanrockjean@gmail.com>
2014-02-20 10:24:39 -0300

    Erro ao selecionar campos da tabela

--- models/post_model.php
+++ models/post_model.php
@@ -2,29 +2,35 @@
 
 class Post_model extends My_model {
 
-    private $__numeroposts = 5;
+    private $__numeroposts = 10;
 
 	public function __construct() {
-		parent::__construct();
+        parent::__construct();
 	}
 
 	public function get_posts($seq = 1) {
-        $query = $this->db->order_by('datacadastro','desc');
+        $query = $this->db->select('*');
+        $query->order_by('post.datacadastro','desc');
+        $query->join('user','post.iduser = user.iduser');
         $query->order_by('idpost','desc');
-        $query->get('post', 
+        return $query->get('post', 
             0 + ( $this->__numeroposts * $seq -1 ) , // inicio
-            $this->__numeroposts * $seq); // fim
-        return $query->result_array();
+            $this->__numeroposts * $seq)->result_array();
 	}
 
-	public function get_post($id) {
-		$query = $this->db->where('idpost', $id);
+    public function get_post($id) {
+        $query = $this->db->select('*');
+        $query->where('idpost', $id);
+        $query->join('user','post.iduser = user.iduser');
+        $query->order_by('post.datacadastro','desc');
 		return $query->get('post')->row();
 	}
 	
 	public function get_posts_iduser($id, $seq = 1) {
-        $query = $this->db->where('iduser', $id);
-        $query->order_by('datacadastro','desc');
+        $query = $this->db->select('*');
+            #'idpost','titulo','texto','iduser','nomeusuario','post.datacadastro');
+        $query->where('iduser', $id);
+        $query->order_by('post.datacadastro','desc');
         $query->order_by('idpost','desc');
         return $query->get(
             'post', 
